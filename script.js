cc0bbbb2cb7bb0e59f3c0ba36621b364aba8f061
gabriellatrujillor <gabriella.trujillor@gmail.com>
2020-05-03 18:23:59 -0600

    all of the js and html work but something happened to my API

--- script.js
+++ script.js
@@ -4,10 +4,11 @@
   //grab lon and lat and get uv vis data(go through to get the value from the obj)
   //dynamically append to html
   //re-enable 5day fx 
+  // HAVE IF STATMENET FOR NULL - if searchbox element == return
 
 
 
-var currWeatherKey = "7eb31c225242f6ad31616765e19c7464";
+var currWeatherKey = "dac12dbdd66b32822c9dde5d1340c3e7";
 var lon;
 var lat;
 var query = "austin";
@@ -60,7 +61,7 @@ btn.attr("id", "cityBtn");
   btn.attr("value", localcityArray[i])
   //<button id="cityBtn" value="austin"></button>
   btn.text(localcityArray[i]);
-  //<button id="cityBtn" value="austin">austin</button>
+    //<button id="cityBtn" value="austin">austin</button>
   $(".buttonarea").append(btn);
 
   }
@@ -71,6 +72,7 @@ btn.attr("id", "cityBtn");
     event.preventDefault();
     // onclick fx triggers when user clicks on citybtn
   //then get the value for that city = $(this).val()
+
   console.log(($(this).val()));
     //call 5day
     fiveday($(this).val());
@@ -85,6 +87,9 @@ btn.attr("id", "cityBtn");
   
 
 }
+
+
+
 
 
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
@@ -103,6 +108,13 @@ function getWeather(query) {
     console.log(response);
     console.log("name:" +response.name);
     $("#current-city-weather").text(response.name);
+    var temp = respone.main.temp;
+    var tempF = (temp-273) * 1.8 + 32;
+    $("#Temp").text("Temperature: " + tempF.toFixed() + " F");
+    $("#Humidity").text("Humidity " + response.main.humidity + " %");
+    $("#WindSpeed").text("Wind Speed: " + response.wind.speed + "mph");
+
+
     lat = response.coord.lat;
     lon = response.coord.lon;
     //http://api.openweathermap.org/data/2.5/uvi/forecast?appid={appid}&lat={lat}&lon={lon}&cnt={cnt}
@@ -119,52 +131,16 @@ function getWeather(query) {
       method: "GET",
     }).then(function (uvobj) {
       console.log(uvobj);
+      $("#uvIndex").text("UV Index")
     });
   });
-  // // Here we are building the URL we need to query the database (one call api)
-  // var weatherQURL =
-  //   "https://api.openweathermap.org/data/2.5/onecall?" +
-  //   "lat={lat}&lon={lon}&appid=" +
-  //   currWeatherKey;
-  // console.log(weatherQURL);
-  // // Here we run our AJAX call to the OpenWeatherMap One Call API
-  // $.ajax({
-  //   url: weatherQURL,
-  //   method: "GET",
-  // }).then(function (response) {
-  //   lon = response.coord.lon;
-  //   lat = response.coord.lat;
-  //   // Log the queryURL
-  //   console.log(weatherQURL);
-  //   // Log the resulting object
-  //   console.log(response);
-  //   //   add city name content to html (would also like to display corresponding icon that i noticed in api response example)
-  //   $("city").html("<h1>" + response.name + " Weather Details</h1>");
-  //   // add temp content to html
-  //   $("#temp").text("Temperature (K) " + response.current.temp);
-  //   $("#tempF").text("Temperature (F) " + tempF.toFixed(2));
-  //   //   Transfer  rest of content to HTML
-  //   $("#humidity").text("Humidity: " + response.current.humidity);
-  //   $("#wind").text("Wind Speed: " + response.current.wind_speed);
-  //   //   $.ajax({
-  //   //     url: https://api.openweathermap.org/data/2.5/onecall?" +
-  //   //     "lat="+lat+"&lon="+lon+"&appid=" +currWeatherKey;
-  //   //     method: "GET",
-  //   //   }).then(function (uvvisobj) {
-  //   //   });
-  //   $("#uvIndex").text("UV Index: " + response.current.uvi);
-  //   // Convert the temp to fahrenheit
-  //   var tempF = (response.current.temp - 273.15) * 1.8 + 32;
-  //   // Log the data in the console as well
-  //   console.log("Wind Speed: " + response.current.wind_speed);
-  //   console.log("Humidity: " + response.current.humidity);
-  //   console.log("Temperature (F): " + tempF);
-  // });
+ 
 }
+
 getWeather("austin");
 function fiveday(query) {
   // 5 day forecast api from OpenWeather
-  var forecastApiKey = "e0b9b9e2f453ce41eb68d5ed75d073ff";
+  var forecastApiKey = "13414e8c24e3fa7f23ad04f011cb28f5";
   var forecastQURL =
     "https://api.openweathermap.org/data/2.5/forecast?" +
     "q=" +
@@ -177,16 +153,7 @@ function fiveday(query) {
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    // <div class="card">
-    //      <div class="card-header">Date</div>
-    //      <div class="card-body">
-    //ADD THIS INTO D3!!!! <p><img src="23.png"></p>
-    //           <p class="card-text">temp</p>
-    //           <p class="card-text"> humidity</p>
-    //      </div>
-    // </div>
-    
-    
+    $("#forecast-sec").empty();
 
     for (var i = 0; i < 5; i++) {
       var d1 = $("<div>");
@@ -203,11 +170,14 @@ function fiveday(query) {
       var p1 = $("<p>");
       //<p></p>
       p1.attr("class", "card-text");
-      p1.text("temp: " + response.list[i * 8].main.temp);
+      var temp5Day = response.list[i * 8].main.temp;
+      var temp5DayF = (temp5Day - 273) * 1.8 + 32;
+
+      p1.text("Temp: " + temp5DayF.toFixed() + " F");
       var p2 = $("<p>");
       //<p></p>
       p2.attr("class", "card-text");
-      p2.text("humidity: " + response.list[i * 8].main.humidity);
+      p2.text("Humidity: " + response.list[i * 8].main.humidity);
       d3.append(p1);
       //<div class="card-body">
       //<p class="card-text">temp</p>
@@ -219,7 +189,7 @@ function fiveday(query) {
       //</div>
       d1.append(d2);
       d1.append(d3);
-      $("#forecast-sec").append(d1);
+      $("#forecast-sec").prepend(d1);
       //date
       console.log(response.list[0].dt_txt);
       var date = response.list[0].main.temp;
@@ -231,10 +201,8 @@ function fiveday(query) {
       console.log(response.list[0].main.humidity);
     }
     $(".forecast").html("<h2>" + "5-Day Forecast</h2>");
-    //   how do i make sure this shows up in the correct spot on the html page?
   });
 }
-//fiveday("austin");
 
 cityButton();
 
